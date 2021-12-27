ddfc07394e9fcdcfe1c57d3f8027db91709ed33c
unknown <arthur.mariano@protonmail.com>
2021-12-26 22:10:55 -0500

    First version of main without solenoid

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,75 +1,156 @@
+//////////////////////////////////////////////////////////////////////////
+/// Name        : main.ino
+/// Description : Principal code for drinking water dispenser
+/// Author      : Arthur MARIANO
+/// Github      : https://github.com/arthurmio/Drinking-Water-Dispenser
+/// Date        : 26/12/2021
+//////////////////////////////////////////////////////////////////////////
+
 #include <Arduino.h>
 #include <Time.h>
 
-//Time
+///////////////////
+// Instantiation //
+///////////////////
+
+// Time //
 volatile float currentTime = 0.00;
 volatile float lastTime = 0.00;
+volatile float start = 0.00;
+volatile float coinsValueSavedTime = 0.00;
 
-//const int buttonPin = ?;
-
-// Flow Meter
-const int flowMeterPin = 2; 
+// Flow Meter //
+const int flowMeterPin = 2; // interrupt port
 const float factorK = 7.5;
 volatile int pulse_freq = 0 ;
 volatile float flow = 0.00;
 volatile float waterVolume = 0.00;
+volatile float flowCounter = 0.00;
 
-// Solenoid Valve
-//const int solenoidPin = ?; 
+// Change Machine //
+const int changeMachinePin = 3; // interrupt port
+volatile int coinsChange = 0;
+volatile float coinsValue = 0.00;
+volatile float coinsValueSaved = 0.00;
+volatile float moneyValue = 0.00;
+//volatile bool payed = false;
+volatile int payed = 0;
+
+// Solenoid //
+const int solenoidPin = 4; 
 volatile int valve = 0;
 
-// Change Machine
-const int changeMachinePin = 3;
-int coinsChange = 1;
-volatile float coinsValue = 0.00;
 
-
+///////////
+// setup //
+///////////
 void setup() {
-  pinMode(flowMeterPin, INPUT); 
-  Serial.begin(115200);
-  attachInterrupt(digitalPinToInterrupt(flowMeterPin), pulseFlow, RISING); // Setup Interrupt (interrupt pin in Nano is 2&3 / ex:https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/)
-  attachInterrupt(digitalPinToInterrupt(changeMachinePin), pulseMoney, RISING);
+  pinMode(flowMeterPin, INPUT);
+  pinMode(changeMachinePin, INPUT); 
+  pinMode(solenoidPin, OUTPUT);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(flowMeterPin), pulseFlow, RISING);
+  attachInterrupt(digitalPinToInterrupt(changeMachinePin), pulseMachine, RISING);
   currentTime = millis();
   lastTime = currentTime;
-  //bool button;
 }
 
-void loop() {
+
+//////////
+// loop //
+//////////
+void loop(){
+  // money calcul
   currentTime = millis();
-  if(currentTime >= (lastTime + 1000)) // count always the flow every second 
-  {   
+  if(currentTime <= (start+1000)){
+    coinsValueSaved = coinsValue;
+    coinsValueSavedTime = millis();
+  }
+  else{
+    coinsValue = 0;
+  }
+  if(currentTime >= (coinsValueSavedTime+1000)){
+    if(coinsValueSaved>=5 && coinsValueSaved <= 11){
+      moneyValue = 1; // 0.50 sole
+      solenoidOpen(moneyValue); 
+    }
+    else if(coinsValueSaved>11){
+      moneyValue = 2; // 1 sole
+      solenoidOpen(moneyValue);
+    }
+    else{
+      moneyValue = 0; // no money
+      solenoidOpen(moneyValue);
+    }   
+  }
+  flowCounter=0.00;
+  flow=0.00;
+  pulse_freq=0.00;
+  while(payed==1){
+    currentTime = millis();
+    if(currentTime >= (lastTime + 1000)) // count always the flow every second 
+    {   
       lastTime = currentTime; 
-      // Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min.
-      flow = (pulse_freq / factorK); // flow in L/min
+      flow = (pulse_freq / factorK) / 60; // flow in L/min
       pulse_freq = 0; // Reset Counter
       Serial.print(flow, DEC); 
       Serial.println(" L/Min");
-
-      // Change Machine
-      
-
-      // Water Volume Counter (a changer en fonction du temps pour avoir uniquement le volume)
-      if(valve==1)
-        waterVolume = volume(flow);
-      else
-        waterVolume = 0;
+      flowCounter = flowCounter + flow;
+      Serial.print(flowCounter, DEC); 
+      Serial.println(" L");
+      flowCount(moneyValue, flowCounter);
+    }
   }
 }
 
-// calcul of water volume
-float volume(float flow)
-{
-  waterVolume += flow;
-  return waterVolume;
-}
+///////////////
+// Functions //
+///////////////
 
-void pulseFlow() // interrupt fonction for flowMeter
+// Flow Meter //
+void pulseFlow() // interrupt fonction for flow meter
 {
   pulse_freq++;
 }
 
-void pulseMoney() // interrupt fonction for Change Machine
+void flowCount(int moneyValue, float flowCounter){ // calcul for volume
+  if(moneyValue == 1){
+    if(flowCounter >= 0.50){
+    valve = 0;
+    payed = 0;
+    Serial.println("0.50L water served");
+    }
+  }
+  else if(moneyValue == 2){
+    if(flowCounter >= 1){
+    valve = 0;
+    payed = 0;
+    Serial.println("1L water served");
+    }
+  }
+  coinsValueSaved=0;
+}
+
+// Change Machine //
+void pulseMachine() // interrupt fonction for change machine
 {
-  coinsValue += 0.5;
-  coinsChange = 1;  
+  coinsValue  = coinsValue + 0.5;
+  start = millis();
+}
+
+// Solenoid //
+void solenoidOpen(int moneyValue){
+  if(moneyValue==1){ // 0.50sole
+    //Serial.println("0.50Sole");
+    payed = 1;
+    valve = 1;
+  }
+  else if(moneyValue==2){ // 1sole
+    //Serial.println("1Sole");
+    payed = 1;
+    valve = 1;
+  }
+  else
+    payed = 0;
+    valve = 0;
 }
